e5e91a5cd55f8f5c490e58ed054dbf1b8964b38d
Adrian Melo <adrian.mb97@gmail.com>
2018-11-08 22:38:02 -0600

    Finished and created tester for player

--- card_demo.cpp
+++ card_demo.cpp
@@ -9,12 +9,8 @@
 
 using namespace std;
 
-
 // PROTOTYPES for functions used by this demonstration program:
 void dealHand(Deck &d, Player &p, int numCards);
-
-
-
 
 int main( ) {
 ////////CARD TESTER//////////
@@ -58,13 +54,12 @@ int main( ) {
 
 ////////PLAYER TESTER//////////
 
-    int numCards = 5;
+    int numCards = 14;
 
     Player p1("Adrian");
     Player p2("David");
 
     Deck d;  //create a deck of cards
-    d.shuffle();
 
     dealHand(d, p1, numCards);
     dealHand(d, p2, numCards);
@@ -74,12 +69,42 @@ int main( ) {
     cout << p1.getName()<< endl;
     cout << p2.getName()<< endl;
 
-    cout << p1.showHand() << endl;
+    cout << "Adrian's Hand: " << p1.showHand() << endl;
 
-    cout << p1.getHandSize() << endl;
+    cout << "Adrian's Cards in Hand: " <<  p1.getHandSize() << endl;
 
-    cout << p1.getBookSize() << endl;
+    cout << "Adrian's Cards in Book: " <<  p1.getBookSize() << endl;
 
+    //Checking Pair and Booking the Card//
+        //Check Hand Tester//
+            Card* card1 = new Card();
+            Card* card2 = new Card();
+
+            if(p1.checkHandForBook(*card1, *card2)){
+                cout << "Adrian has a pair!" << endl;
+            } else{
+                cout << "Adrian doesn't have a pair!" << endl;
+            }
+
+        //Book Cards Tester//
+            p1.bookCards(*card1, *card2);
+            cout << "Adrian's Book: " << p1.showBooks() << endl;
+
+        //Remove Hand Tester//
+            p1.removeCardFromHand(*card1);
+            p1.removeCardFromHand(*card2);
+            cout << "Adrian's Hand: " << p1.showHand() << endl;
+
+    //Rank in Hand Tester//
+        if(p1.rankInHand(Card(1,Card::spades))){
+            cout << "Adrian has the rank!" << endl;
+        }
+        if(p1.cardInHand(Card(2,Card::spades))){
+            cout << "Adrian has the card!" << endl;
+        }
+
+    //Have to create a function to iterate through the hand to check pairs to be able to book any pairs
+    d.shuffle();
 
 ////////DEMO TESTER//////////
 
--- player.cpp
+++ player.cpp
@@ -24,43 +24,52 @@ void Player::bookCards(Card c1, Card c2){
     myBook.push_back(c1);
     myBook.push_back(c2);
 }
-//
-////OPTIONAL
-//// comment out if you decide to not use it
+
 ////this function will check a players hand for a pair.
 ////If a pair is found, it returns true and populates the two variables with the cards tha make the pair.
-//bool Player::checkHandForBook(Card &c1, Card &c2){
-//
-//}
-//
-//
-///OPTIONAL
-/// comment out if you decide to not use it
+bool Player::checkHandForBook(Card &c1, Card &c2){
+    for(int i = 0; i < getHandSize(); i++){
+        for (int j = i+1; j < getHandSize() ; ++j) {
+            if(myHand[i].getRank()==myHand[j].getRank()){
+                //Setting card so these can get used to pass to book and remove from hand after booking
+                c1 = myHand[i];
+                c2 = myHand[j];
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
 ///Does the player have a card with the same rank as c in her hand?
-
-//bool Player::rankInHand(Card c) const{
-//
-//}
-
-
+bool Player::rankInHand(Card c) const{
+    for(int i = 0; i< getHandSize(); i++){
+        if(myHand[i].getRank()==c.getRank()){
+            return true;
+        }
+    }
+    return false;
+}
 
 
 ////uses some strategy to choose one card from the player's
 ////hand so they can say "Do you have a 4?"
-
 Card Player::chooseCardFromHand() const{
     //should we sort the cards then go through them individually?
-
     return(myHand[0]);
-
 }
-
 
 ////Does the player have the card c in her hand?
 bool Player::cardInHand(Card c) const{
-
+    for(int i = 0; i< getHandSize(); i++){
+        if(myHand[i]==c){
+            return true;
+        }
+    }
+    return false;
 }
-
+////PreCondition for calling this method is that the card must be in there hand.
 ////Remove the card c from the hand and return it to the caller
 Card Player::removeCardFromHand(Card c){
     Card temp(1, Card::spades);
@@ -69,9 +78,9 @@ Card Player::removeCardFromHand(Card c){
         if(c == myHand[i]){
             temp = myHand[i];
             myHand.erase((myHand.begin())+i);
-            return temp;
         }
     }
+    return temp;
 }
 
 string Player::showHand() const{
@@ -85,7 +94,13 @@ string Player::showHand() const{
 }
 
 string Player::showBooks() const{
+    vector<Card>::const_iterator iter;
+    string hand;
 
+    for(iter = (myBook.begin()); iter!=myBook.end(); iter++){
+        hand+= iter->toString();
+    }
+    return hand;
 }
 
 int Player::getHandSize() const{
@@ -101,11 +116,13 @@ int Player::getHandSize() const{
 
 int Player::getBookSize() const{
     vector<Card>::const_iterator iter;
-    int size = 0;
+    int cardSize = 0;
 
     for(iter = (myBook.begin()); iter!=myBook.end(); iter++){
-        size ++;
+        cardSize ++;
     }
 
-    return size;
+    int bookSize = cardSize/2; // Size divided by two as books are pairs
+
+    return bookSize;
 }
